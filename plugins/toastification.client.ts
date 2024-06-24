cd804c11ebeb626b8fc8b202ff74053150b95b9c
Marvellous Sam-kayode <marvelloussamkayode@Marvellouss-MacBook-Air.local>
2024-06-24 08:45:42 +0100

    add changes

--- plugins/toastification.client.ts
+++ plugins/toastification.client.ts
@@ -1,6 +1,6 @@
 import { defineNuxtPlugin } from '#app'
 import * as vt from 'vue-toastification'
-import '@/assets/css/toast.scss'
+import 'vue-toastification/dist/index.css';
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(vt.default)
